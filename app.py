d1db6351a1febf0d4973b62661115ccc0a8d9dd4
mihjovil <ma.caldas331@gmail.com>
2021-10-11 07:21:01 +0200

    :art: Changed to Flask factory pattern

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
 from logic import internal_class
-app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    internal = internal_class()
-    return render_template('index.html', name=internal.name)
+def create_app():
+    app = Flask(__name__)
 
+    @app.route('/')
+    def hello_world():
+        internal = internal_class()
+        return render_template('index.html', name=internal.name)
 
-if __name__ == '__main__':
-    app.run()
+    if __name__ == '__main__':
+        app.run()
+    return app
 
 # region help for deployment
 """
